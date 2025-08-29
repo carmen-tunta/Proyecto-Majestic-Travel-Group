e4fe1dcf5157cf358ef0f6cb1b07867cd1e21881
alejandro-tunta <alejandro@tunta.com.bo>
2025-08-29 17:40:14 -0400

    Se hizo la autenticacion desde el frontend

--- server/src/config/database.module.ts
+++ server/src/config/database.module.ts
@@ -5,17 +5,11 @@ import { User } from '../modules/users/entities/user.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '195.250.27.197',
-      port: 3306,
-      username: 'd9202_usuario',
-      password: 'X94K_#ZNEOBQ%4L_',
-      database: 'd9202_db1',
-      /*host: process.env.DB_HOST,
+      host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '3306'),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,*/
+      database: process.env.DB_NAME,
       entities: [User], // Aquí se agregarán las entidades
       synchronize: true, // Cambia a true solo en desarrollo
     }),
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,13 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Habilitar CORS para permitir peticiones del frontend
+  app.enableCors({
+    origin: 'http://localhost:3000', // Frontend
+    credentials: true,
+  });
+  
+  await app.listen(3001); // Puerto específico para el backend
 }
 bootstrap();
